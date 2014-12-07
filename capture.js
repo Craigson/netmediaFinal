e2522e854f71b0ea36d5f8d40a14e4e9c1e9168c
Craigson <craigpick@gmail.com>
2014-12-07 15:04:20 -0500

    fixed canvas position problem

--- capture.js
+++ capture.js
@@ -3,15 +3,13 @@ var capture;
 function setup(){
 var myCanvas = createCanvas(320,240);
 capture = createCapture(VIDEO);
-
-//myCanvas.position(0,0);
-//myCanvas.class("takePicture");
-//myCanvas.parent('myContainer');
+capture.size(320,240);
+myCanvas.parent('myContainer');
+capture.hide();
 
 }
-
-
 function draw(){
+image(capture,0,0,320,240);
 
 }
     
