6cd4473b6063e400f4db362719f515f748b9c12b
Craigson <craigpick@gmail.com>
2014-12-07 14:48:55 -0500

    initial commit

---
+++ capture.js
@@ -0,0 +1,18 @@
+var capture;
+
+function setup(){
+var myCanvas = createCanvas(320,240);
+capture = createCapture(VIDEO);
+
+//myCanvas.position(0,0);
+//myCanvas.class("takePicture");
+//myCanvas.parent('myContainer');
+
+}
+
+
+function draw(){
+
+}
+    
+
